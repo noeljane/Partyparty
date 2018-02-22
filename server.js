6ae35dee9f68eea198080b933cf18e85698df890
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-02-21 18:42:22 -0800

    add special heroku script to package.json

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ const
     app = express(),
     PORT = process.env.PORT || 3001
 
+//Use the client build folder
 app.use(express.static(`${__dirname}/client/build`))
 
+//Root
 app.get('/api', (req,res) => {
     res.json({message: "root"})
 })
 
+//WildCard- Catches all random routes
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
+//Server Listen
 app.listen(PORT, (err) =>{
     console.log(err || `Server running on port ${PORT}.`)
 })
