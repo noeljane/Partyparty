70d79d23f8e2e80049eb090841b6cf325858458b
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-02-22 11:06:19 -0800

    added routes for users

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,13 @@ const
 
 usersRouter.route('/')
     .get(usersCtrl.index)
+    .post(usersCtrl.create)
 
+usersRouter.post('/authenticate', usersCtrl.authenticate)
+
+usersRouter.route('/:id')
+    .get(usersCtrl.show)
+    .patch(usersCtrl.update)
+    .delete(usersCtrl.destroy)
+    
 module.exports = usersRouter
