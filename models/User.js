bb3d7f114f44a890c029740b9d26e40c06c8a672
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-02-21 22:28:06 -0800

    write methods for user model

--- models/User.js
+++ models/User.js
@@ -4,5 +4,26 @@ const
     userSchema = new mongoose.Schema({
         name: { type: String },
         email: { type: String, required: true, unique: true }, 
-        
+
     })
+
+    //Encrypts Your Password when it is generated
+    userSchema.methods.generateHash = function(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+    }
+
+    //Checks to see if your password is correct
+    userSchema.methods.generateHash = function(password) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+    }
+
+    //Middleware for changing password
+    userSchema.pre('save', function(next) {
+        if(this.isModified('password')){
+            this.password = this.generateHash(this.password)
+        }
+        next()
+    })
+
+    const User = mongoose.model('User', userSchema)
+    module.exports = User
