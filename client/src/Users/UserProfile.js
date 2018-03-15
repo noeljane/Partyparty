656e762af9cee64e45e09bc2dcc32b3659b46001
Noel Jane Bourg <noeljanebourg@gmail.com>
2018-03-15 16:20:44 -0700

    Your parties only 1x on UserProfile

--- client/src/Users/UserProfile.js
+++ client/src/Users/UserProfile.js
@@ -10,7 +10,6 @@ class UserProfile extends React.Component {
             <div>
                 {/* <img alt="Koala dancing to a boombox" src="https://media2.giphy.com/media/3o6fJ47X7rsRR9wDlu/200w.webp" /> */}
                 <h1 id={this.props.user.id}>Your Parties: </h1> 
-                    <h3>Your Parties:</h3>
                         <PartyIndex />
 
                     <h3>Parties I'm Attending: </h3>
